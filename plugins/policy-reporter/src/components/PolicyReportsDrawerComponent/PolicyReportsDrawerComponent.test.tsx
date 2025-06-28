1625a351b00bda932b2275c3ee463e1d6f260945
Jonas Beck <dev@jonasbeck.dk>
2025-06-28 21:02:54 +0200

    test(drawer): add test case to drawer
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>

--- plugins/policy-reporter/src/components/PolicyReportsDrawerComponent/PolicyReportsDrawerComponent.test.tsx
+++ plugins/policy-reporter/src/components/PolicyReportsDrawerComponent/PolicyReportsDrawerComponent.test.tsx
@@ -25,4 +25,42 @@ describe('PolicyReportsDrawerComponent', () => {
 
     expect(extension.getAllByText('Policy1')).toHaveLength(1);
   });
+
+  it('should render table displaying the policy details with additonal properties', async () => {
+    const data: ListResult = {
+      id: '0',
+      kind: 'deployment',
+      namespace: 'default',
+      name: 'name',
+      resourceId: 'id',
+      message: 'Policy 1 passed successfully',
+      timestamp: 123456789,
+      policy: 'Policy1',
+      rule: 'Rule1',
+      status: 'pass',
+      severity: 'low',
+      properties: {
+        key: 'value',
+      },
+    };
+
+    const extension = await renderInTestApp(
+      <PolicyReportsDrawerComponent content={data} />,
+    );
+
+    expect(extension.getAllByText('Policy1')).toBeTruthy();
+    expect(extension.getAllByText('key')).toBeTruthy();
+    expect(extension.getAllByText('value')).toBeTruthy();
+    expect(extension.getAllByText('Additional Properties')).toBeTruthy();
+  });
+
+  it('should render message if policy is undefined', async () => {
+    const extension = await renderInTestApp(
+      <PolicyReportsDrawerComponent content={undefined} />,
+    );
+
+    expect(
+      extension.getAllByText('No policy information available'),
+    ).toBeTruthy();
+  });
 });
