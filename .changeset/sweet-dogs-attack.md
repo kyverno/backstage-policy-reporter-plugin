c4e5ae40cd25323562aa7d30e8f307b3b2949107
Jonas Beck <112394761+Jonas-Beck@users.noreply.github.com>
2026-03-16 09:15:26 +0100

    feat(NFS): add initial support for the new frontend system (#95)
    * feat(nfs): add alpha export
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>
    
    * feat(nfs): add initial NFS plugin
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>
    
    * feat(nfs): add migrated app for local development
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>
    
    * feat(nfs): add initial extensions for page and entity content
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>
    
    * chore(catalog): add component without policy reporter annotations
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>
    
    * chore(app): add NFS package
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>
    
    * chore(changeset): add changeset
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>
    
    ---------
    
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>

---
+++ .changeset/sweet-dogs-attack.md
@@ -0,0 +1,5 @@
+---
+'@kyverno/backstage-plugin-policy-reporter': minor
+---
+
+Add initial support for the new Frontend System, this will be available under a `/alpha` route when importing packages.
---
+++ plugins/policy-reporter/src/alpha/plugin.tsx
@@ -0,0 +1,99 @@
+import {
+  ApiBlueprint,
+  createFrontendPlugin,
+  createRouteRef,
+  discoveryApiRef,
+  fetchApiRef,
+  NavItemBlueprint,
+  PageBlueprint,
+  SubPageBlueprint,
+} from '@backstage/frontend-plugin-api';
+
+import { rootRouteRef } from '../routes';
+import { policyReporterApiRef } from '../api';
+import { DefaultApiClient } from '@kyverno/backstage-plugin-policy-reporter-common';
+
+import { EntityContentBlueprint } from '@backstage/plugin-catalog-react/alpha';
+import { isPolicyReporterAvailable } from '../utils/annotations';
+import PolicyIcon from '@material-ui/icons/Policy';
+
+/** @alpha */
+export const policyReporterApi = ApiBlueprint.make({
+  params: defineParams =>
+    defineParams({
+      api: policyReporterApiRef,
+      deps: {
+        discoveryApi: discoveryApiRef,
+        fetchApi: fetchApiRef,
+      },
+      factory: ({ discoveryApi, fetchApi }) =>
+        new DefaultApiClient({ discoveryApi, fetchApi }),
+    }),
+});
+
+/** @alpha */
+const policyReporterPage = PageBlueprint.make({
+  params: {
+    path: '/policy-reporter',
+    routeRef: rootRouteRef,
+    title: 'Policy Reporter',
+  },
+});
+
+/** @alpha */
+const policyReporterNamespacedPoliciesPage = SubPageBlueprint.make({
+  name: 'namespaced-policy',
+  params: {
+    path: 'namespaced-policies',
+    routeRef: createRouteRef(),
+    // TODO: Support configuration of the actual component
+    title: 'Namespaced Policies',
+    loader: () =>
+      // TODO: Create dedicated PolicyReporterNamespacedPoliciesPage Component
+      import('../components/PolicyReportsPage/').then(m => (
+        <m.PolicyReportsPage /> // TODO: Update the PolicyReportsPage to be only used for old frontend system to mimic PageBlueprint from NFS
+      )),
+  },
+});
+
+/** @alpha */
+const policyReporterEntityContent = EntityContentBlueprint.make({
+  name: 'policy-reporter',
+  params: {
+    path: '/policy-reporter',
+    title: 'Policy Reporter',
+    // TODO: Support configuration of the actual component
+    filter: isPolicyReporterAvailable,
+    loader: () =>
+      // TODO: Create dedicated EntityPolicyReporterContent component as replacement for Kyverno and Custom content components
+      import('../components/EntityKyvernoPoliciesContent/').then(m => (
+        <m.EntityKyvernoPoliciesContent />
+      )),
+  },
+});
+
+/** @alpha */
+export const policyReporterNavItem = NavItemBlueprint.make({
+  params: {
+    title: 'Policy Reporter',
+    icon: () => <PolicyIcon />,
+    routeRef: rootRouteRef,
+  },
+});
+
+/** @alpha */
+export default createFrontendPlugin({
+  pluginId: 'policy-reporter',
+  title: 'Policy Reporter',
+  info: { packageJson: () => import('../../package.json') },
+  routes: {
+    root: rootRouteRef,
+  },
+  extensions: [
+    policyReporterApi,
+    policyReporterEntityContent,
+    policyReporterPage,
+    policyReporterNamespacedPoliciesPage,
+    policyReporterNavItem,
+  ],
+});
