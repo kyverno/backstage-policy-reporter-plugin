1e49836553e962bd5dec7677287cee5433b90b3d
Jonas Beck <dev@jonasbeck.dk>
2026-03-31 08:06:51 +0200

    feat(search): add useDebounce hook
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>

--- plugins/policy-reporter/src/components/SearchField/SearchField.tsx
+++ plugins/policy-reporter/src/components/SearchField/SearchField.tsx
@@ -1,26 +1,34 @@
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { useDebounce } from 'react-use';
 import { SearchField as BackstageSearchField } from '@backstage/ui';
 
 export const SearchField = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const searchValue = searchParams.get('search') ?? '';
+  const [inputValue, setInputValue] = useState(
+    searchParams.get('search') ?? '',
+  );
 
-  const handleChange = (value: string) => {
-    const newParams = new URLSearchParams(searchParams);
-    if (value) {
-      newParams.set('search', value);
-    } else {
-      newParams.delete('search');
-    }
-    setSearchParams(newParams, { replace: true });
-  };
+  useDebounce(
+    () => {
+      const newParams = new URLSearchParams(searchParams);
+      if (inputValue) {
+        newParams.set('search', inputValue);
+      } else {
+        newParams.delete('search');
+      }
+      setSearchParams(newParams, { replace: true });
+    },
+    300,
+    [inputValue],
+  );
 
   return (
     <BackstageSearchField
       aria-label="Search"
       label="Search"
-      value={searchValue}
-      onChange={handleChange}
+      value={inputValue}
+      onChange={setInputValue}
       style={{ width: '350px' }}
     />
   );
