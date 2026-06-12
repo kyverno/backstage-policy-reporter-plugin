c9e79c2036c4e30877b54b7ddb7f23c2fb6d0cd6
Jonas Beck <dev@jonasbeck.dk>
2026-06-12 14:00:18 +0200

    fix(yarn): update script for changeset to use correct yarn path
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>

--- .github/changeset-version.cjs
+++ .github/changeset-version.cjs
@@ -2,5 +2,5 @@ const { execSync } = require("node:child_process");
 
 execSync("yarn changeset version");
 execSync(
-  "YARN_ENABLE_IMMUTABLE_INSTALLS=false node .yarn/releases/yarn-4.5.0.cjs",
+  "YARN_ENABLE_IMMUTABLE_INSTALLS=false node .yarn/releases/yarn-4.10.3.cjs",
 );
